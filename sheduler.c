dfad7e125dcc3bc12359b27eb7d9dc51f7b0e344
Radchenko Evgeniy <captain.r97@gmail.com>
2019-10-07 21:56:59 +1000

    Some fixes

--- sheduler.c
+++ sheduler.c
@@ -2,12 +2,31 @@
 
 void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
 {
+	// Triggers 1000 times per second (once per millisecond)
+	static uint16_t cnt = 0;
+	static uint16_t tick = 0;
+	
 	if (htim->Instance == TIM2 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
 	{
-		battery_calculate_voltage();
-	}
-	else if (htim->Instance == TIM2 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
-	{
-		void copter_handle_actuators();
+		cnt++;
+		if (!(cnt % ADC_HANLING_INTERVAL_MS))
+		{
+			battery_calculate_voltage();
+		}
+		if (!(cnt %  BT_MSG_HANDLING_INTERVAL_MS))
+		{
+			copter_handle_actuators();
+		}
+		if (!(cnt % IMU_HANDLING_INTERVAL_MS))
+		{
+			tick = HAL_GetTick();
+			calculate_orientation();
+			uint16_t a = HAL_GetTick() - tick;
+			uint16_t b = 0;
+		}
+		if (!(cnt % CNT_RESET))
+		{
+			cnt = 0;
+		}
 	}
 }
--- sheduler.h
+++ sheduler.h
@@ -4,5 +4,11 @@
 #include "stm32f1xx_hal.h"
 #include "battery.h"
 #include "copter.h"
+#include "IMU.h"
+
+#define ADC_HANLING_INTERVAL_MS			1000
+#define BT_MSG_HANDLING_INTERVAL_MS		50
+#define IMU_HANDLING_INTERVAL_MS		5
+#define CNT_RESET						1000
 
 #endif /*__sheduler_H */
